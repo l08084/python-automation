5d224206ef99aa683cad21e59f97a87486274146
l08084 <l08084.1989@gmail.com>
2019-09-03 18:05:12 +0900

    update

--- download_d.py
+++ download_d.py
@@ -5,22 +5,14 @@ from selenium import webdriver
 import chromedriver_binary
 from d_credentials import *
 
-
-# パスワードの指定
+# ユーザIDとパスワードを別ファイルから取得する
 user_id = ID
 password = PASS
-download_dir = os.path.dirname(__file__)
 
-# 保存先などオプションを指定してChromeを起動
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option("prefs", {
-    "download.default_directory": download_dir,
-    "download.prompt_for_download": False,
-    "plugin.always_open_pdf_externally": True
-})
-driver = webdriver.Chrome(options=opt)
+# Chromeを起動
+driver = webdriver.Chrome()
 
-# カード会社のページを開く
+# Dカードの利用明細についてのページを開く
 driver.get('https://d-card.smt.docomo.ne.jp/tknsrv?strURL=https%3A%2F%2Fwww5.dcmx.jp%2Fdcmx%2Fmeisai%3FprocessCode%3D03Meisai%26pn%3D1#_ga=2.194953003.729480172.1567333046-1119956819.1567333046')
 time.sleep(3)
 
@@ -34,13 +26,9 @@ u = driver.find_element_by_name('authpass')
 u.send_keys(password)
 u.submit()
 
-# データのダウンロードリンク先を取得
-time.sleep(5)
-# tag = driver.find_element_by_link_text('CSVをダウンロードする')
+# 今月の利用明細CSVをダウンロードする
 driver.execute_script("doSubmitForm(document.download_form)")
-# driver.execute_script(
-#     "document.querySelector('.csv-print_bottom p a').click();")
 
-# 結果を30秒表示して終了する
+# 30秒待ってから終了する
 time.sleep(30)
 driver.quit()
